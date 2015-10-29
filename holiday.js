3533a905f87c1f90cbe28449cca3424804e6b51d
Robert (Bobby) Ayers <rsheldon3ayers@gmail.com>
2015-10-29 12:39:30 -0500

    first draft

---
+++ holiday.js
@@ -0,0 +1,61 @@
+var HolidayGreeting = (function(){
+	
+	var klingon = {
+		
+		"happy": "Quch",
+		"new": "chu'",
+		"year": "DIS"
+
+	};
+	console.log(klingon);
+	var elvish = {
+		
+		"happy": "Iderin",
+		"new": "eden",
+		"year": "'elir"
+
+	};
+	var jabberwocky = {
+
+		"happy": "frabjous",
+		"new": "warrble",
+		"year": "tove"
+
+
+	};
+	return {
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return HolidayGreeting;
+
+})();
+console.log("HolidayGreeting");
